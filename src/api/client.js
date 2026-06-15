4ebbb13a6bd0eeab2cefabc7a7ce529d9c389eeb
prachibhopale02 <prachibhopale02032gmail.com>
2026-06-15 17:05:49 +0530

    new commit

--- src/api/client.js
+++ src/api/client.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:5002/api',
+  baseURL: 'https://escapeverse-backend.onrender.com/api',
   headers: { 'Content-Type': 'application/json' }
 });
 
